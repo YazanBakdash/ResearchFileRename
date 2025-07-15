d7c1db1bf59c9aab045255ce5c2caa430fd362ba
Yazan Bakdash <yazanbakdash@gmail.com>
2025-07-14 23:14:06 -0500

    now looks for code in names

--- rename.py
+++ rename.py
@@ -5,59 +5,34 @@ data_dir = Path("data copy")#Path("2021")
 df = pd.read_excel("Legend for LOAD100.xlsx")
 mapping = dict(zip(df['Subject ID #'],df['Sort Order #']))
 
-def rename_file(file, is_subdir):
-    name = file.name
-    l = 0
-    if is_subdir:
-        while name[l] != "_":
-            l += 1
-        l += 1
-    else:
-        while name[l] != "k":
-            l += 1
-        l += 2
-    r = l
-    while name[r] != "_":
-        r += 1
-    try:
-        old_id = int(name[l:r])
-        new_id = mapping[old_id]
-    except (KeyError, ValueError) as e:
-        print(f"Skipping file {file.name}: {e}")
-        return -1
-    new_name = name[:l] + "#" + str(new_id) + name[r:]
-    new_path = file.with_name(new_name)
-    file.rename(new_path)
-    print(f"Renamed {name} to {new_name}")
-    return 0
+def rename(dir):
+    name = dir.name
 
-def rename_file(file):
-    name = file.name
-    l = 0
-    while name[l] != "k":
-        l += 1
-    l += 2
-    r = l
-    while name[r] != "_":
-        r += 1
-    try:
-        old_id = int(name[l:r])
-        new_id = mapping[old_id]
-    except (KeyError, ValueError) as e:
-        print(f"  Skipping file {file.name}: {e}")
+    new_name = ""
+    for i in range(len(name)-4):
+        if i+7 <= len(name) and name[i:i+7] == "63995_b":
+            new_name = name[:i] + "#78" + name[i+7:]
+            break
+        
+        s = name[i:i+5]
+        if s.isdigit() and int(s) in mapping:
+            new_id = mapping[int(s)]
+            new_name = name[:i] + "#" + str(new_id) + name[i+5:]
+    
+    if new_name == "":
         return -1
-    new_name = name[:l] + "#" + str(new_id) + name[r:]
-    new_path = file.with_name(new_name)
-    #file.rename(new_path)
-    print(f"  Renamed {name} to {new_name}")
+    
+    new_path = dir.with_name(new_name)
+    dir.rename(new_path)
+    print(f"   Renamed {name} to {new_name}")
     return 0
 
 for subdir in data_dir.iterdir():
     if not subdir.is_dir():
         continue
-    print(f"{subdir}:")
+    print(f"Checking '{subdir}':")
     for file in subdir.iterdir():
-        if file.is_dir():
+        if file.is_dir() or file.suffix != ".raw":
             continue
-        rename_file(file)
-    #rename_file(subdir, True)
+        rename(file)
+    rename(subdir)
